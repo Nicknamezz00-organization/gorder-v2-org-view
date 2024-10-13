be051e1abd84aa88d8c1b2fdfc70a299d9b90e7e
wrz <1315098969@qq.com>
2024-10-14 00:49:20 +0800

    add app to servers && air

---
+++ internal/order/service/application.go
@@ -0,0 +1,11 @@
+package service
+
+import (
+	"context"
+
+	"github.com/Nicknamezz00/gorder-v2/order/app"
+)
+
+func NewApplication(ctx context.Context) app.Application {
+	return app.Application{}
+}
