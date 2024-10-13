addc77c0927978892e6a6a8e1b5d5e2bedffd33a
wrz <1315098969@qq.com>
2024-10-14 00:22:45 +0800

    http, grpc 服务搭建

--- internal/order/main.go
+++ internal/order/main.go
@@ -4,7 +4,12 @@ import (
 	"log"
 
 	"github.com/Nicknamezz00/gorder-v2/common/config"
+	"github.com/Nicknamezz00/gorder-v2/common/genproto/orderpb"
+	"github.com/Nicknamezz00/gorder-v2/common/server"
+	"github.com/Nicknamezz00/gorder-v2/order/ports"
+	"github.com/gin-gonic/gin"
 	"github.com/spf13/viper"
+	"google.golang.org/grpc"
 )
 
 func init() {
@@ -14,5 +19,19 @@ func init() {
 }
 
 func main() {
-	log.Printf("%v", viper.Get("order"))
+	serviceName := viper.GetString("order.service-name")
+
+	go server.RunGRPCServer(serviceName, func(server *grpc.Server) {
+		svc := ports.NewGRPCServer()
+		orderpb.RegisterOrderServiceServer(server, svc)
+	})
+
+	server.RunHTTPServer(serviceName, func(router *gin.Engine) {
+		ports.RegisterHandlersWithOptions(router, HTTPServer{}, ports.GinServerOptions{
+			BaseURL:      "/api",
+			Middlewares:  nil,
+			ErrorHandler: nil,
+		})
+	})
+
 }
