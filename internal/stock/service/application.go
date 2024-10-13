fa71dd612733061d51ad2fd8b7aff74ce4f48069
wrz <1315098969@qq.com>
2024-10-14 00:41:57 +0800

    add app to servers

---
+++ internal/stock/service/application.go
@@ -0,0 +1,11 @@
+package service
+
+import (
+	"context"
+
+	"github.com/Nicknamezz00/gorder-v2/stock/app"
+)
+
+func NewApplication(ctx context.Context) app.Application {
+	return app.Application{}
+}
