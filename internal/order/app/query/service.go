9e01dc2106b36eb8e22727be86c14cece7b246fb
wrz <1315098969@qq.com>
2024-10-15 01:31:13 +0800

    stock query and grpc

--- internal/order/app/query/service.go
+++ internal/order/app/query/service.go
@@ -4,9 +4,10 @@ import (
 	"context"
 
 	"github.com/Nicknamezz00/gorder-v2/common/genproto/orderpb"
+	"github.com/Nicknamezz00/gorder-v2/common/genproto/stockpb"
 )
 
 type StockService interface {
-	CheckIfItemsInStock(ctx context.Context, items []*orderpb.ItemWithQuantity) error
+	CheckIfItemsInStock(ctx context.Context, items []*orderpb.ItemWithQuantity) (*stockpb.CheckIfItemsInStockResponse, error)
 	GetItems(ctx context.Context, itemIDs []string) ([]*orderpb.Item, error)
 }
